f654b03c412a70d4da1dc7368cd69b57e6f7f309
Alec Yu <alecyu@Alecs-MacBook-Pro.local>
2021-11-17 11:22:01 -0500

    Implement pPOS and dPOS

--- Models.py
+++ Models.py
@@ -1,47 +1,47 @@
 import random as rd
-from pylab import *
+import numpy as np
 
+class node:
 
-"""
-agents represent individual miners. 
-They can 
-    1)give tokens to stake
-    2)receive reward for their staking
-    3)receive random network reward
+    def __init__(self, id, wealth):
+        self.id = id
+        self.wealth = wealth
+        self.blocks = 1
 
-Agents are organized in a network whose topology can be altered. 
-    1)can be used to model who delegate to who
-    2)can be used in attack models
+    def update(self, reward):
+        self.wealth += reward
+        self.blocks += 1
 
-Since the real network usually contains about 80-90k max users, 
-we model with ~100 agents for visualization and ~1k for simulation
+class network:
 
-"""
+    def __init__(self, num_nodes):
+        self.nodes = []
+        self.num_nodes = num_nodes
+        self.normal_distribution()
 
+    def number_of_nodes(self):
+        return self.num_nodes
 
-# create an agent with a mood with 
-class agent:
-        
-    def __init__(self, i):
-        self.tokens = random()*100  # bound (0,100)  round(random(),2)
-        self.index= i
-     
-    def stake_give(self,pool,amount): #have their stake locked 
-        self.tokens -= amount
-        pool.tokens += amount #pool is a special kind of agent
+    def gini_coefficient(self):
+        """Compute Gini coefficient of array of values"""
+        x = np.array([n.wealth for n in self.nodes])
+        diffsum = 0
+        for i, xi in enumerate(x[:-1], 1):
+            diffsum += np.sum(np.abs(xi - x[i:]))
+        return diffsum / (len(x)**2 * np.mean(x))
 
-    def stake_reward(self, amount): #reward after being selected (models the transaction fee earned) 
-        self.tokens += amount
-    
-    def network_reward(self,amount): #reward from the network (models the block reward)
-        self.token += amount
+    def real_distribution(self):
+        for low in range(int(self.num_nodes*0.8)):
+            wealth = np.random.normal(loc=10, scale=2)
+            self.nodes.append(node(low, wealth))
+        for med in range(int(self.num_nodes*0.15)):
+            wealth = np.random.normal(loc=100, scale=10)
+            self.nodes.append(node(int(self.num_nodes*0.8)+med, wealth))
+        for high in range(int(self.num_nodes*0.05)):
+            wealth = 1000
+            self.nodes.append(node(int(self.num_nodes*0.95)+high, wealth))
 
-
-    
-def initialize_network_agent(network):
-    directory = {} 
-    node_num = network.number_of_nodes()
-    for i in range (node_num):
-        directory.update({ i : agent(i)})
-    return directory
-
+    def normal_distribution(self):
+        for i in range(self.num_nodes):
+            wealth = np.random.normal(loc=100, scale = 10)
+            self.nodes.append(node(i, wealth))
---
+++ protocol.py
@@ -0,0 +1,43 @@
+from os import replace
+import random as rd
+import numpy as np
+import collections
+
+class protocol_interface:
+    def validate_block(network, num_validators):
+        """Choose a number of validators from the network and reward them"""
+        pass
+
+
+class pPOS(protocol_interface):
+    def __init__(self, network):
+        self.network = network
+
+    def validate_block(self, num_validators):
+        wealth_values = [n.wealth for n in self.network.nodes]
+        total = sum(wealth_values)
+
+        # winner selected psudo-randomly with weight proportional to wealth
+        probability_distribution = [w/total for w in wealth_values]
+        winners = list(np.random.choice(self.network.nodes, num_validators, p=probability_distribution, replace=False))
+        for w in winners:
+            reward = 20 + rd.randint(6,12)
+            w.update(reward)
+
+class dPOS(protocol_interface):
+    def __init__(self, network):
+        self.network = network
+
+    def validate_block(self, num_validators):
+        block_values = [n.blocks for n in self.network.nodes]
+        total = sum(block_values)
+
+        # each node votes for another node to be a validator
+        # nodes vote psudo-randomly with weight proportional to number of blocks validated
+        probability_distribution = [w/total for w in block_values]
+        election = list(np.random.choice(self.network.nodes, self.network.num_nodes, p=probability_distribution, replace=True))
+        top_votes = collections.Counter(election).most_common()[:num_validators]
+        winners = [n[0] for n in top_votes]
+        for w in winners:
+            reward = 20 + rd.randint(6,12)
+            w.update(reward)
