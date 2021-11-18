170be330a848051d1d4bc78e622f9707abdcf3ce
Jiajia <sieglinde_hu@icloud.com>
2021-11-18 12:22:06 -0500

    update readme and analytics

--- decentralization_analytics.py
+++ decentralization_analytics.py
@@ -16,3 +16,33 @@ beta = k/n, measure how beta changed
 - among people participate, who are likely to win
 - % of wealth (NEED MORE DISCUSSION)
 '''
+
+
+"""
+ ---------------------------   1 Gini ---------------------------
+https://stackoverflow.com/questions/39512260/calculating-gini-coefficient-in-python-numpy
+"""
+
+"""
+1.Gini Coefficient
+the Gini coefficient is half the average difference of incomes between each all possible pairs of individuals, divided by the mean income
+Or 
+The space between y = x and he Lorenz curve -  share of total income earned by the lowest-earning portion of the population 
+
+2. Theil L index
+Capture the change of total utility 
+Utility function U(x) = Log(x), and calculate the change of 
+Sum Log (X_i)
+
+3.Herfindahl-Hirschman index
+Measure the economic concentration 
+Sum(X^2)/(Sum X_i)^2
+
+
+3. Nakamoto coefficient: 
+the minimum number of participants needed to add up to more than 50% of the total
+
+- visualize each how the income change over time with scattered plot
+
+- 
+"""
--- security.py
+++ security.py
@@ -2,8 +2,21 @@
 Consensus Layer
 xBFT - how to ensure information get 
 
-- Majority attack (double spend, private chain)
-- nothing at stack 
+
+Attack 
+1.fairness in e-POS papr: prevent fraud and fairly compensate if attack 
+denote H as total honest stake
+denote A as total adersay stake
+
+- Majority Attack, double spend
+
+- Majority Attack, private chain dominate
+
+- Nothing at Stake (usually caused by 
+
+- blocking transactions 
+
 - stake theft 
+
 
 """
